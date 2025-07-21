55369fb6e6e997358dbaa9300618806e1e5c7d34
Syed Asghar <syedasghar178@gmail.com>
2025-07-21 11:42:22 -0400

    small css changes

--- src/Componets/SideBar.jsx
+++ src/Componets/SideBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function SideBar () {
     return (
-        <div>
+        <div className="side-bar-container">
             <Link to="/"><button>Home</button></Link>
             <Link to="/logs"><button>Logs</button></Link>
             <Link to="/food"><button>Food</button></Link>
